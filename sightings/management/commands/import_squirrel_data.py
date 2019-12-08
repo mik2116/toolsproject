64475591d4ab21229768950c1cf18822d2aecf82
Mahmoud Khatib <mik2116@columbia.edu>
2019-12-08 20:30:57 +0000

    merged new changes after pulling

---
+++ sightings/management/commands/import_squirrel_data.py
@@ -0,0 +1,45 @@
+import csv
+from django.core.management.base import BaseCommand, CommandError
+import datetime
+from sightings.models import Tracker
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('path')
+    #comment 
+    def handle(self, *args, **options):
+        with open(options['path']) as fp:
+            reader = csv.DictReader(fp)
+            data = list(reader)
+
+        for item in data:
+            t = Tracker(
+                x = item['X'],
+                y = item['Y'],
+                ID = item['Unique Squirrel ID'],
+                shift = item['Shiftx'],
+                date = item['Date'],
+                age = item['Age'],
+                primary_fur_colo = item['Primary Fur Color'],
+                location = ['Location'],
+                specific_location = ['Specific Location'],
+                running = ['Running'],
+                chasing = ['Chasing'],
+                climbing = ['Climbing'],
+                eating = ['Eating'],
+                foraging = ['Foraging'],
+                other_activities = ['Other Activities'],
+                kuks = ['Kuks'],
+                quaas = ['Quaas'],
+                moans = ['Moans'],
+                tail_flags = ['Tail Flags'],
+                tail_twitches = ['Tail Twiches'],
+                approaches = ['Approaches'],
+                Indifferent = ['Indifferent'],
+                runs_from = ['Runs From'],
+
+
+
+                )   
+            t.save()
